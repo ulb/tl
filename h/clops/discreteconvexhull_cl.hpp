d80b7eedada81980c759d58d750f8f407addc59f
Marco Macchia <macchia.marco90@gmail.com>
2017-10-20 16:50:12 +0200

    dchcl improvements

--- h/clops/discreteconvexhull_cl.hpp
+++ h/clops/discreteconvexhull_cl.hpp
@@ -1,34 +1,47 @@
 #ifndef H_CLOPS_DISCRETECONVEXHULL_CL
 #define H_CLOPS_DISCRETECONVEXHULL_CL
 
-#include <cstring> // std::equal
+#include <cstring> // std::fill
+#include "../alloc.hpp"
 
 namespace clops {
+	template <typename T,typename SIZE>
+	SIZE get_ones(T * char_v,const SIZE length,T *& one_indices) {
+		alloc(one_indices,length,T);
+		SIZE num_one_indices = 0;
+		for (SIZE i = 0; i < length; ++i) if (char_v[i] == 1) one_indices[num_one_indices++] = i;
+		return num_one_indices;
+	} 
+
 	// Compute the discrete convex hull of a point set A
 	template <typename T,typename SIZE>
 	void discreteconvexhull_cl(T * A,T *& B,T *& dchcl,T ** slab_points_sat,const SIZE length_A,const SIZE length_B) {
-	    int i, j;
 	    
 	    // Set all bits of B to 1
 	    std::fill(B,B+length_B,1);
-	    
+
+	    T * A_indices;
+	    SIZE num_A_indices = get_ones(A,length_A,A_indices);
+   
 	    // Intersect all sets of slabs belonging to elements of A
-	    for (i = 0; i < length_A; ++i) {
-	        if (A[i] == 1) {
-	            for (j = 0; j < length_B; ++j)
-	                if (slab_points_sat[i][j] == 0)  B[j] = 0;
-	        }
-	    }    
+	    for (SIZE j = 0; j < length_B; ++j) {
+	    	for (SIZE i = 0; i < num_A_indices; ++i)
+	    		B[j] &= slab_points_sat[A_indices[i]][j];
+	    }
+	    free(A_indices);
+
 	    // Set all bits of dchcl to 1
 	    std::fill(dchcl,dchcl+length_A,1);
+
+	    T * B_indices;
+	    SIZE num_B_indices = get_ones(B,length_B,B_indices);
 	    
 	    // Intersect all sets of points belonging to elements of B
-	    for (i = 0; i < length_B; ++i) {
-	        if (B[i] == 1) {
-	            for (j = 0; j < length_A; ++j)
-	                if (slab_points_sat[j][i] == 0) dchcl[j] = 0;
-	        }
+	    for (SIZE i = 0; i < length_A; ++i) {
+	    	for (SIZE j = 0; j < num_B_indices; ++j)
+	    		dchcl[i] &= slab_points_sat[i][B_indices[j]];
 	    }
+	    free(B_indices);
 	}
 }
 
