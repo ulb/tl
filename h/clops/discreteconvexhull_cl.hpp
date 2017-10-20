ee869345f39a72afbc5d489bdfd4f71f1f719845
Marco Macchia <macchia.marco90@gmail.com>
2017-10-20 18:00:52 +0200

    fallback

--- h/clops/discreteconvexhull_cl.hpp
+++ h/clops/discreteconvexhull_cl.hpp
@@ -13,17 +13,17 @@ namespace clops {
 		return num_one_indices;
 	}
 
-	template <typename T,typename SIZE>
-	SIZE get_idx(T * char_v,const SIZE length,T *& one_indices,T *& zero_indices) {
-		alloc(one_indices,length,T);
-		alloc(zero_indices,length,T);
-		SIZE num_one_indices = 0;
-		SIZE num_zero_indices = 0;
-		for (SIZE i = 0; i < length; ++i)
-			if (char_v[i] == 1) one_indices[num_one_indices++] = i;
-			else zero_indices[num_zero_indices++] = i;
-		return num_one_indices;
-	} 
+	// template <typename T,typename SIZE>
+	// SIZE get_idx(T * char_v,const SIZE length,T *& one_indices,T *& zero_indices) {
+	// 	alloc(one_indices,length,T);
+	// 	alloc(zero_indices,length,T);
+	// 	SIZE num_one_indices = 0;
+	// 	SIZE num_zero_indices = 0;
+	// 	for (SIZE i = 0; i < length; ++i)
+	// 		if (char_v[i] == 1) one_indices[num_one_indices++] = i;
+	// 		else zero_indices[num_zero_indices++] = i;
+	// 	return num_one_indices;
+	// } 
 
 	// Compute the discrete convex hull of a point set A
 	template <typename T,typename SIZE>
@@ -32,8 +32,8 @@ namespace clops {
 	    // Set all bits of B to 1
 	    std::fill(B,B+length_B,1);
 
-	    T * A_indices, * not_A_indices;
-	    SIZE num_A_indices = get_idx(A,length_A,A_indices,not_A_indices);
+	    T * A_indices;//, * not_A_indices;
+	    SIZE num_A_indices = get_ones(A,length_A,A_indices);
    
 	    // Intersect all sets of slabs belonging to elements of A
 	    for (SIZE j = 0; j < length_B; ++j) {
@@ -49,11 +49,12 @@ namespace clops {
 	    SIZE num_B_indices = get_ones(B,length_B,B_indices);
 	    
 	    // Intersect all sets of points belonging to elements of B
-	    for (SIZE i = 0; i < length_A - num_A_indices; ++i) {
-	    	for (SIZE j = 0; j < num_B_indices; ++j)
-	    		dchcl[not_A_indices[i]] &= slab_points_sat[not_A_indices[i]][B_indices[j]];
+	    for (SIZE i = 0; i < length_A; ++i) {
+	    	if (A[i] == 0) {
+		    	for (SIZE j = 0; j < num_B_indices; ++j)
+		    		dchcl[i] &= slab_points_sat[i][B_indices[j]];
+	    	}
 	    }
-	    free(not_A_indices);
 	    free(B_indices);
 	}
 }
