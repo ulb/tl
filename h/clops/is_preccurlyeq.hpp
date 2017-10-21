128dba6878b9fd34dfa178efb47314fb68572afd
Marco Macchia <macchia.marco90@gmail.com>
2017-10-21 21:44:39 +0200

    minor updates

--- h/clops/is_preccurlyeq.hpp
+++ h/clops/is_preccurlyeq.hpp
@@ -1,13 +1,11 @@
 #ifndef H_CLOPS_IS_PRECCURLYEQ
 #define H_CLOPS_IS_PRECCURLYEQ
 
-#include "../array/is_equal.hpp"
-
 namespace clops {
 	// Check if A \preccurlyeq B
 	template <typename T,typename SIZE>
 	bool is_preccurlyeq(T* A, T* B, const SIZE length) {
-		T* a(A+length); T* b(B+length);
+		T* a(A+length), * b(B+length);
 	    while (a != A) if (*(--a) != *(--b)) return *b == 1;
 	    return true;
 	}
