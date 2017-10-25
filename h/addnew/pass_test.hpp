685d2269ad9bf92f0533258a0ee3add6a600eb44
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-10-25 16:45:50 +0200

    add explanation and clean addnew::pass_test

--- h/addnew/pass_test.hpp
+++ h/addnew/pass_test.hpp
@@ -2,15 +2,16 @@
 #define H_ADDNEW_PASS_TEST
 
 namespace addnew {
-	// check if the slack matrix S is already listed in LD; if not, add it to LD //int *& LD_hash, setword **& LD
+	/**
+	 * Counts the number of vertices in each facet. Must be <= to the number of
+	 * vertices of the base. Should be replaced by checking that each face that
+	 * is not the base is lexicographically larger than the base.
+	 */
 	template <typename T,typename SIZE>
-	bool pass_test(T ** S_new, const SIZE num_rows_S_new, const SIZE num_cols_S_new, const SIZE num_cols_S) {
-	    int i,j;
-	    SIZE num_vertices_Fi;
-	    for (i = 0; i < num_rows_S_new; ++i) {
-	        num_vertices_Fi = 0;
-	        for (j = 0; j < num_cols_S_new; ++j)
-	            if (S_new[i][j] == 0) num_vertices_Fi++;
+	bool pass_test(T** S_new, const SIZE num_rows_S_new, const SIZE num_cols_S_new, const SIZE num_cols_S) {
+	    for (int i = 0; i < num_rows_S_new; ++i) {
+	        SIZE num_vertices_Fi = 0;
+	        for (int j = 0; j < num_cols_S_new; ++j) if (S_new[i][j] == 0) ++num_vertices_Fi;
 	        if (num_vertices_Fi > num_cols_S) return false;
 	    }
 	    return true;
