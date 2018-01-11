960a70dd042e507f280d87b2f8a2ab05b007297f
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-01-11 23:52:41 +0100

    branchless get_ones and pointer-based max_col_sat

--- h/array/get_ones.hpp
+++ h/array/get_ones.hpp
@@ -8,20 +8,22 @@ namespace array {
 	SIZE get_ones(T* char_v,const SIZE length,T*& one_indices) {
 		alloc(one_indices,length,T);
 		SIZE num_one_indices = 0;
-		for (SIZE i = 0; i < length; ++i) if (char_v[i] == 1) one_indices[num_one_indices++] = i;
+		for (SIZE i = 0; i < length; ++i) {
+			one_indices[num_one_indices] = i;
+			num_one_indices += char_v[i];
+		}
 		return num_one_indices;
 	}
 
 	template <typename SIZE,typename T>
-	SIZE get_ones_64(uint64_t* word,const SIZE nwords,T*& one_indices, const SIZE length) {
+	SIZE get_ones_64(uint64_t* word,const SIZE length,T*& one_indices) {
 		alloc(one_indices,length,T);
 		SIZE num_one_indices = 0;
-		for (SIZE i = 0; i < nwords; ++i) {
-			for (SIZE j = 0; j < 64; ++j) {
-				auto k = i*64+j;
-				if (k==length) break;
-				if ( ( word[i] >> j ) & 1 ) one_indices[num_one_indices++] = k;
-			}
+		for (SIZE k = 0; k < length; ++k) {
+			const auto i = k / 64 ;
+			const auto j = k % 64 ;
+			one_indices[num_one_indices] = k;
+			num_one_indices += ( ( word[i] >> j ) & 1 );
 		}
 		return num_one_indices;
 	}
--- h/clops/fast_discreteconvexhull_cl.hpp
+++ h/clops/fast_discreteconvexhull_cl.hpp
@@ -11,12 +11,13 @@ namespace clops {
 	template <typename T, typename W, typename SIZE, typename SP>
 	void max_col_sat(T* P_ones, const SIZE P_nones, W* Q, const SIZE n, SP M) {
 
-		std::fill(Q,Q+n,~W(0)); // fill Q with all 1's
+		auto Qn = Q + n;
+		std::fill(Q,Qn,~W(0)); // fill Q with all 1's
 
 		for (SIZE i = 0; i < P_nones; ++i) {
-			auto P_indices_i = P_ones[i];
-			auto M_i = M[P_indices_i];
-			for (SIZE j = 0; j < n; ++j) Q[j] &= M_i[j];
+			auto Mij = M[P_ones[i]];
+			auto Qj = Q;
+			while (Qj != Qn) *(Qj++) &= *(Mij++);
 		}
 
 	}
@@ -25,20 +26,17 @@ namespace clops {
 	// Compute the discrete convex hull of a point set A
 	template <typename T, typename W,typename SIZE, typename SP>
 	void fast_discreteconvexhull_cl(T* A, W* B, W* C, SP sp, SP sp_t, const SIZE n_rows,const SIZE n_cols,const SIZE n_rows_64,const SIZE n_cols_64) {
+
 		SIZE *A_ones;
 		const SIZE A_nones = array::get_ones(A,n_rows,A_ones);
-		// std::cerr << ANSI_RED << "A ------------------ " << n_rows << ' ' << n_rows_64 << ANSI_RESET << std::endl;
-		// array::dump(A, n_rows);
-		// array::dump(A_ones, A_nones);
 		clops::max_col_sat(A_ones,A_nones,B,n_cols_64,sp);
 		free(A_ones);
+
 		SIZE *B_ones;
-		const SIZE B_nones = array::get_ones_64(B,n_cols_64,B_ones,n_cols);
-		// std::cerr << ANSI_GREEN << "B ------------------ "  << n_cols << ' ' << n_cols_64 << ANSI_RESET << std::endl;
-		// array::dump_64(B, n_cols_64);
-		// array::dump(B_ones, B_nones);
+		const SIZE B_nones = array::get_ones_64(B,n_cols,B_ones);
 		clops::max_col_sat(B_ones,B_nones,C,n_rows_64,sp_t);
 		free(B_ones);
+
 	}
 }
 
