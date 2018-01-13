e44f4a4bcbd32913388828e06143c0dad93c171f
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-01-13 13:44:23 +0100

    ceci: use global scratch and independent space

--- job.sh
+++ job.sh
@@ -4,52 +4,42 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=3
 #SBATCH --job-name=tl-#d-#base
-#SBATCH -o hpc/log/#d/#base.out
-#SBATCH -e hpc/log/#d/#base.err
+#SBATCH -o #GLOBALSCRATCH/tl/#d/log/#base.out
+#SBATCH -e #GLOBALSCRATCH/tl/#d/log/#base.err
 
 d="#d"
 base="#base"
-lockfile="$LOCALSCRATCH/.tl-lockfile"
-lockdfile="$LOCALSCRATCH/.tl-lockdfile"
-scratch="$LOCALSCRATCH/.tl"
-workspaces="$scratch/$d"
-workspace="$workspaces/$base"
+workspace="$LOCALSCRATCH/.tl-$d-$base"
 run="$workspace/run"
 bases="$workspace/bases"
 polytopes="$workspace/polytopes"
+src_run="$CECIHOME/tl/run"
+src_bases="$CECIHOME/tl/db/src/$d"
+flag_fail="$GLOBALSCRATCH/tl/$d/fail/$base"
+flag_done="$GLOBALSCRATCH/tl/$d/done/$base"
+output="$GLOBALSCRATCH/tl/$d/out/$base"
 
 >&2 echo "d=$d"
 >&2 echo "base=$base"
->&2 echo "lockfile=$lockfile"
->&2 echo "lockdfile=$lockdfile"
->&2 echo "scratch=$scratch"
->&2 echo "workspaces=$workspaces"
 >&2 echo "workspace=$workspace"
 >&2 echo "run=$run"
 >&2 echo "bases=$bases"
 >&2 echo "polytopes=$polytopes"
+>&2 echo "src_run=$src_run"
+>&2 echo "src_bases=$src_bases"
+>&2 echo "flag_fail=$flag_fail"
+>&2 echo "flag_done=$flag_done"
+>&2 echo "output=$output"
 
 function _do {
 	>&2 echo "$@"
 	eval "$@"
 }
 
-exec {lock}>"$lockfile"
-_do flock -x "$lock"
-if [ ! -e "$scratch" ] ; then _do mkdir "$scratch" ; fi
-_do flock -u "$lock"
-exec {lock}>&-
-
-exec {lockd}>"$lockdfile"
-_do flock -x "$lockd"
-if [ ! -e "$workspaces" ] ; then _do mkdir "$workspaces" ; fi
-_do flock -u "$lockd"
-exec {lockd}>&-
-
 _do rm -rf "$workspace"
 _do mkdir -p "$workspace"
-_do cp -r "$CECIHOME/tl/run" "$run"
-_do cp "$CECIHOME/tl/db/src/$d" "$bases"
+_do cp -r "$src_run" "$run"
+_do cp "$src_bases" "$bases"
 
 _do module load GCCcore
 
@@ -61,8 +51,10 @@ tail "-n+$base" "$bases" | head -n1 |
 "$run/dedup" 2>/dev/null |
 "$run/sift" 2>/dev/null "$bases" > "$polytopes"
 
-_do cp "$polytopes" "$CECIHOME/tl/hpc/out/$d/$base"
+if [ "$?" -ne 0 ] ; then _do touch "$flag_fail" ; fi
 
-_do touch "$CECIHOME/tl/hpc/done/$d/$base"
+_do cp "$polytopes" "$output"
+
+_do touch "$flag_done"
 
 _do rm -r "$workspace"
--- manager.sh
+++ manager.sh
@@ -1,22 +1,52 @@
+function now {
+	date '+%Y-%m-%d_%H:%M:%S'
+}
+
+START_TIME="$SECONDS"
+START_DATE="$(now)"
+
+function info {
+	>&2 echo "[$(now)]" "$@"
+}
+
 d="$1"
 n="$(wc -l < "db/src/$d")"
 
 slots=200
 interval=5
-jobs="hpc/jobs/$d"
-queue="hpc/queue/$d"
-running="hpc/running/$d"
-done="hpc/done/$d"
-out="hpc/out/$d"
-log="hpc/log/$d"
-resume="hpc/resume"
+scratch="$GLOBALSCRATCH/tl/$d"
+jobs="$scratch/jobs"
+queue="$scratch/queue"
+running="$scratch/running"
+done="$scratch/done"
+out="$scratch/out"
+log="$scratch/log"
+resume="$scratch/resume"
 
-if [ -e "$resume" ] ; then >&2 echo "Resuming ..." ; else
+function variables {
+	info "START_DATE=$START_DATE"
+	info "d=$d"
+	info "n=$n"
+	info "slots=$slots"
+	info "interval=$interval"
+	info "scratch=$scratch"
+	info "jobs=$jobs"
+	info "queue=$queue"
+	info "running=$running"
+	info "done=$done"
+	info "out=$out"
+	info "log=$log"
+	info "resume=$resume"
+}
+
+variables
+
+if [ -e "$resume" ] ; then info "Resuming ..." ; else
 
 	# assumes no other manager is running
 	dangling="$(squeue -h -u "$USER" -o "%j %i" | grep -E "^tl-$d-[1-9][0-9]*" | cut -d' ' -f2)"
 	if [ "$dangling" != '' ] ; then scancel $dangling ; fi
-	rm -rf hpc
+	rm -rf "$scratch"
 
 	mkdir -p "$jobs"
 	mkdir -p "$queue"
@@ -42,8 +72,12 @@ function count {
 function submit {
 	base="$1"
 	job="$jobs/$base.sh"
-	sed -e "s/#d/$d/g" -e "s/#base/$base/g" job.sh > "$job"
-	>&2 echo "tl-$d-$base submission"
+	sed \
+		-e "s/#d/$d/g" \
+		-e "s/#base/$base/g" \
+		-e "s/#GLOBALSCRATCH/$GLOBALSCRATCH/g" \
+		job.sh > "$job"
+	info "tl-$d-$base submission"
 	sbatch "$job" && touch "$running/$base" && rm "$queue/$base"
 }
 
@@ -54,20 +88,20 @@ while true ; do
 
 	if [ "$finished" -ge "$n" ] ; then break ; fi
 
-	>&2 echo "$finished/$n jobs finished"
+	info "$finished/$n jobs finished"
 
 	for base in $(list "$running"); do
 		if [ -e "$done/$base" ] ; then
-			>&2 echo "tl-$d-$base done"
+			info "tl-$d-$base done"
 			rm "$running/$base"
 		fi
 	done
 
 	nrunning="$(count "$running")"
-	>&2 echo "$nrunning jobs running"
+	info "$nrunning jobs running"
 
 	available="$(($slots-$nrunning))"
-	>&2 echo "$available slots available"
+	info "$available slots available"
 
 	started=0
 	for base in $(list "$queue") ; do
@@ -76,9 +110,22 @@ while true ; do
 		started="$(($started+1))"
 	done
 
-	>&2 echo "sleeping $interval seconds"
+	info "sleeping $interval seconds"
 	sleep "$interval"
 
 done
 
->&2 echo "All jobs finished"
+STOP_TIME="$SECONDS"
+
+SECONDS="$(($STOP_TIME-$START_TIME))"
+SEC="$(($SECONDS%60))"
+MINUTES="$(($SECONDS/60))"
+MIN="$(($MINUTES%60))"
+HOURS="$(($MINUTES/60))"
+H="$(($HOURS%24))"
+DAYS="$(($HOURS/24))"
+D="$(($DAYS))"
+
+>&2 echo "All jobs finished in $D days $H hours $MIN minutes and $SEC seconds."
+
+variables
