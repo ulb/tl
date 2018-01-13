b454c406c574b101874e76e74616ef79b5f5a1c5
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-01-13 12:17:56 +0100

    improve cluster scripts

--- job.sh
+++ job.sh
@@ -9,26 +9,24 @@
 
 d="#d"
 base="#base"
-lockfile="$LOCALSCRATCH/.tl-prepare"
-lockdfile="$LOCALSCRATCH/.tl-prepare-$d"
-tl="$LOCALSCRATCH/tl"
-run="$tl/run"
-db="$tl/db"
-bases="$db/$d"
-out="$tl/out/$d"
-workspace="$out/$base"
+lockfile="$LOCALSCRATCH/.tl-lockfile"
+lockdfile="$LOCALSCRATCH/.tl-lockdfile"
+scratch="$LOCALSCRATCH/.tl"
+worpskaces="$scratch/$d"
+workspace="$workspaces/$base"
+run="$workspace/run"
+bases="$workspace/bases"
 polytopes="$workspace/polytopes"
 
 >&2 echo "d=$d"
 >&2 echo "base=$base"
 >&2 echo "lockfile=$lockfile"
 >&2 echo "lockdfile=$lockdfile"
->&2 echo "tl=$tl"
->&2 echo "run=$run"
->&2 echo "db=$db"
->&2 echo "bases=$bases"
->&2 echo "out=$out"
+>&2 echo "scratch=$scratch"
+>&2 echo "workspaces=$workspaces"
 >&2 echo "workspace=$workspace"
+>&2 echo "run=$run"
+>&2 echo "bases=$bases"
 >&2 echo "polytopes=$polytopes"
 
 function _do {
@@ -38,25 +36,25 @@ function _do {
 
 exec {lock}>"$lockfile"
 _do flock -x "$lock"
-if [ ! -e "$tl" ] ; then _do mkdir "$tl" ; fi
-if [ ! -e "$db" ] ; then _do mkdir "$db" ; fi
-if [ ! -e "$run" ] ; then _do cp -r "$CECIHOME/tl/run" "$run" ; fi
+if [ ! -e "$scratch" ] ; then _do mkdir "$scratch" ; fi
 _do flock -u "$lock"
 exec {lock}>&-
 
 exec {lockd}>"$lockdfile"
 _do flock -x "$lockd"
-if [ ! -e "$bases" ] ; then _do cp "$CECIHOME/tl/db/src/$d" "$bases" ; fi
-if [ ! -e "$out" ] ; then _do mkdir -p "$out" ; fi
+if [ ! -e "$workspaces" ] ; then _do mkdir -p "$workspaces" ; fi
 _do flock -u "$lockd"
 exec {lockd}>&-
 
+_do rm -rf "$workspace"
 _do mkdir -p "$workspace"
+_do cp -r "$CECIHOME/tl/run" "$run"
+_do cp "$CECIHOME/tl/db/src/$d" "$bases"
+
+_do module load GCCcore
 
 >&2 echo "Enumerating 2-level polytopes from $d-dimensional base n°$base (writing to $polytopes):"
 tail "-n+$base" "$bases" | head -n1 >&2
-
-module load GCCcore
 
 tail "-n+$base" "$bases" | head -n1 |
 "$run/inflate" 2>/dev/null |
--- manager.sh
+++ manager.sh
@@ -1,54 +1,84 @@
 d="$1"
+n="$(wc -l < "db/src/$d")"
 
-slots=20
-interval=30
+slots=200
+interval=5
 jobs="hpc/jobs/$d"
+queue="hpc/queue/$d"
 running="hpc/running/$d"
 done="hpc/done/$d"
 out="hpc/out/$d"
 log="hpc/log/$d"
+resume="hpc/resume"
 
-mkdir -p "$jobs"
-mkdir -p "$running"
-mkdir -p "$done"
-mkdir -p "$out"
-mkdir -p "$log"
+if [ ! -e "$resume" ] ; then
+
+	# assumes no other manager is running
+	dangling="$(squeue -h -u "$USER" -o "%j %i" | grep -E "^tl-$d-[1-9][0-9]*" | cut -d' ' -f2)"
+	if [ "$dangling" != '' ] ; then scancel $dangling ; fi
+	rm -rf hpc
+
+	mkdir -p "$jobs"
+	mkdir -p "$queue"
+	mkdir -p "$running"
+	mkdir -p "$done"
+	mkdir -p "$out"
+	mkdir -p "$log"
+
+	for i in $(seq 1 "$n") ; do touch "$queue/$i" ; done
+
+	touch "$resume"
+
+fi
+
+function list {
+	find "$1" -type f -printf "%f\n"
+}
+
+function count {
+	list "$1" | wc -l
+}
 
 function submit {
 	base="$1"
 	job="$jobs/$base.sh"
 	sed -e "s/#d/$d/g" -e "s/#base/$base/g" job.sh > "$job"
 	>&2 echo "tl-$d-$base submission"
-	sbatch "$job" && touch "$running/$base"
+	sbatch "$job" && touch "$running/$base" && rm "$queue/$base"
 }
 
-i=1
-n="$(wc -l < db/src/$d)"
 
-while [ "$i" -le "$n" ] ; do
+while true ; do
 
-	for base in $(find "$running" -type f -printf "%f\n"); do
+	finished="$(count "$done")"
+
+	if [ "$finished" -ge "$n" ] ; then break ; fi
+
+	>&2 echo "$finished/$n jobs finished"
+
+	for base in $(list "$running"); do
 		if [ -e "$done/$base" ] ; then
 			>&2 echo "tl-$d-$base done"
 			rm "$running/$base"
 		fi
 	done
 
-	nrunning="$(find "$running" -type f -printf "%f\n" | wc -l)"
+	nrunning="$(count "$running")"
 	>&2 echo "$nrunning jobs running"
 
 	available="$(($slots-$nrunning))"
 	>&2 echo "$available slots available"
 
-	for ((j=0;j<$available;j++)); do
-		base="$(($i+$j))"
-		if [ "$base" -gt "$n" ] ; then break ; fi
+	started=0
+	for base in $(list "$queue") ; do
+		if [ "$started" -ge "$available" ] ; then break ; fi
 		submit "$base"
+		started="$(($started+1))"
 	done
-
-	i="$(($i+$available))"
 
 	>&2 echo "sleeping $interval seconds"
 	sleep "$interval"
 
 done
+
+>&2 echo "All jobs finished"
