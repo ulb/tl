04a40ca9036b2d65a45a4cb580adaf3af145c6ce
Marco Macchia <macchia.marco90@gmail.com>
2018-01-26 11:10:59 +0100

    updates in construct_ground_H

--- h/base/construct_ground_H.hpp
+++ h/base/construct_ground_H.hpp
@@ -8,8 +8,8 @@
 #include <vector> // std::vector
 
 
-#include "mem/alloc.hpp"
 #include "linalg/my_matrix_prod.hpp"
+#include "linalg/my_inner_prod.hpp"
 
 namespace base {
 
@@ -17,14 +17,12 @@ namespace base {
     // Thus, so is the H embedding of the ground set
     template <typename T,typename SIZE>
     bool accept(T ** facets_base,const SIZE num_facets_base,T * point,const SIZE D) {
-        T xE;
         // Facet reduction of the ground set:
         // we can throw away all the points x of the ground set where we do not have x(E) in {-1,0,1}
         // for x(E) >= 0, x(E) <= 1 facet of the base, E subset of {2,...,d}
         for (SIZE i = 0; i < num_facets_base; ++i) {
-            xE = 0;
-            for (SIZE j = 1; j < D; ++j) if (facets_base[i][j] == 1) xE += point[j];
-            if ((xE != -1) && (xE != 0) && (xE != 1)) return false;
+            T xE = linalg::my_inner_prod(facets_base[i],point,D);
+            if (xE != -1 && xE != 0 && xE != 1) return false;
         }
         return true;
     }
@@ -34,15 +32,10 @@ namespace base {
 	void construct_ground_H(T ** ground_H,SIZE & size_ground_H,T ** ground_V,const SIZE size_ground_V,T ** facets_base,const SIZE num_facets_base,T ** Minv,const SIZE D) {
         size_ground_H = 0;
         for (SIZE i = 0; i < size_ground_V; ++i) {
-            T * point;
-            mem::alloc(point,D);
+            T * point = ground_H[size_ground_H];
             linalg::my_matrix_prod(Minv,ground_V[i],point,D,D);
 
-            if (accept(facets_base,num_facets_base,point,D)) {
-                std::memcpy(ground_H[size_ground_H],point,D * sizeof(T));
-                size_ground_H++;
-            }
-            free(point);
+            if (accept(facets_base,num_facets_base,point,D)) ++size_ground_H;
         }
 
 	}
