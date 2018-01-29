c6f04ad9fa770bdea43f67f5c7c7c2b5b9c90df3
Aurélien Ooms <aurelien.ooms@gmail.com>
2018-01-29 15:55:19 +0100

    simplify construct incomp adjM

--- h/base/construct_incompatibility_adjM.hpp
+++ h/base/construct_incompatibility_adjM.hpp
@@ -1,37 +1,24 @@
 #ifndef H_BASE_CONSTRUCT_INCOMPATIBILITY_ADJM
 #define H_BASE_CONSTRUCT_INCOMPATIBILITY_ADJM
 
-#include <cstring> // std::memcpy, std::memset, std::fill
-
-#include "mem/alloc.hpp"
 #include "linalg/my_inner_prod.hpp"
 
 namespace base {
     template <typename T,typename SIZE>
-    bool is_incompat(T ** ground_H, const T i, const T j,T ** facets_base,const SIZE num_facets_base,const T D) {
-        int k,h;
-        T s_i, s_j;
-        for (k = 0; k < num_facets_base; ++k) {
-            s_i = 0;
-            s_j = 0;
-            for (h = 1; h < D; ++h) {
-                if (facets_base[k][h] != 0) {
-                    s_i += ground_H[i][h];
-                    s_j += ground_H[j][h];
-                }
-            }
+    bool is_incompat(const T* i, const T* j, T ** facets_base, const SIZE num_facets_base, const T D) {
+        for (SIZE k = 0; k < num_facets_base; ++k) {
+            T s_i = linalg::my_inner_prod(facets_base[k], i, D);
+            T s_j = linalg::my_inner_prod(facets_base[k], j, D);
             if (s_i * s_j == -1) return true;
         }
         return false;
     }
 
     template <typename T,typename SIZE>
-    void construct_incompatibility_adjM(T ** incompatibility_adjM, T ** ground_H,T ** facets_base,const SIZE size_ground_H,const SIZE num_facets_base,const T D) {
-        int i,j;
-        for (i = 1; i < size_ground_H; ++i) {
-            for (j = 0; j < i; ++j) {
-                if (is_incompat(ground_H,i,j,facets_base,num_facets_base,D)) incompatibility_adjM[i][j] = 1;
-                else incompatibility_adjM[i][j] = 0;
+    void construct_incompatibility_adjM(T ** incompatibility_adjM, T ** ground_H, T ** facets_base, const SIZE size_ground_H, const SIZE num_facets_base, const T D) {
+        for (SIZE i = 1; i < size_ground_H; ++i) {
+            for (SIZE j = 0; j < i; ++j) {
+                incompatibility_adjM[i][j] = is_incompat(ground_H[i],ground_H[j],facets_base,num_facets_base,D) ? 1 : 0 ;
             }
         }
     }
